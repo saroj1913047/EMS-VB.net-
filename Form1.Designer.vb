a8af9c376b2649297452f5fd6c0aa0bf7663dfd4
Saroj Sharma <sharmasaroj531@gmail.com>
2022-03-13 19:22:15 +0545

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,360 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.SystemName = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.DataTable = New System.Windows.Forms.DataGridView()
+        Me.TextPrice = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextAuthor = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextTitle = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.TextSearch = New System.Windows.Forms.TextBox()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.DataTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel5.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1308, 126)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel2.Controls.Add(Me.SystemName)
+        Me.Panel2.Location = New System.Drawing.Point(14, 18)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1278, 92)
+        Me.Panel2.TabIndex = 1
+        '
+        'SystemName
+        '
+        Me.SystemName.AutoSize = True
+        Me.SystemName.Font = New System.Drawing.Font("Verdana", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.SystemName.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.SystemName.Location = New System.Drawing.Point(186, 0)
+        Me.SystemName.Name = "SystemName"
+        Me.SystemName.Size = New System.Drawing.Size(961, 73)
+        Me.SystemName.TabIndex = 0
+        Me.SystemName.Text = "Books Management System"
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel3.Controls.Add(Me.Panel4)
+        Me.Panel3.Location = New System.Drawing.Point(12, 144)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1045, 607)
+        Me.Panel3.TabIndex = 2
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel4.Controls.Add(Me.DataTable)
+        Me.Panel4.Controls.Add(Me.TextPrice)
+        Me.Panel4.Controls.Add(Me.Label4)
+        Me.Panel4.Controls.Add(Me.TextAuthor)
+        Me.Panel4.Controls.Add(Me.Label3)
+        Me.Panel4.Controls.Add(Me.TextTitle)
+        Me.Panel4.Controls.Add(Me.Label2)
+        Me.Panel4.Location = New System.Drawing.Point(14, 19)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(941, 568)
+        Me.Panel4.TabIndex = 1
+        '
+        'DataTable
+        '
+        Me.DataTable.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataTable.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataTable.BackgroundColor = System.Drawing.Color.SkyBlue
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataTable.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataTable.ColumnHeadersHeight = 50
+        Me.DataTable.Location = New System.Drawing.Point(25, 221)
+        Me.DataTable.MultiSelect = False
+        Me.DataTable.Name = "DataTable"
+        Me.DataTable.RowHeadersWidth = 51
+        Me.DataTable.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.DataTable.RowTemplate.Height = 29
+        Me.DataTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataTable.Size = New System.Drawing.Size(896, 336)
+        Me.DataTable.TabIndex = 2
+        '
+        'TextPrice
+        '
+        Me.TextPrice.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextPrice.Location = New System.Drawing.Point(290, 150)
+        Me.TextPrice.Name = "TextPrice"
+        Me.TextPrice.Size = New System.Drawing.Size(631, 61)
+        Me.TextPrice.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(18, 153)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(116, 54)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "Price"
+        '
+        'TextAuthor
+        '
+        Me.TextAuthor.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextAuthor.Location = New System.Drawing.Point(290, 83)
+        Me.TextAuthor.Name = "TextAuthor"
+        Me.TextAuthor.Size = New System.Drawing.Size(631, 61)
+        Me.TextAuthor.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(18, 86)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(155, 54)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Author"
+        '
+        'TextTitle
+        '
+        Me.TextTitle.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.TextTitle.Location = New System.Drawing.Point(290, 16)
+        Me.TextTitle.Name = "TextTitle"
+        Me.TextTitle.Size = New System.Drawing.Size(631, 61)
+        Me.TextTitle.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(18, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 54)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Title"
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel5.Controls.Add(Me.Panel7)
+        Me.Panel5.Controls.Add(Me.Panel6)
+        Me.Panel5.Location = New System.Drawing.Point(953, 144)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(367, 607)
+        Me.Panel5.TabIndex = 3
+        '
+        'Panel7
+        '
+        Me.Panel7.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel7.Controls.Add(Me.TextSearch)
+        Me.Panel7.Location = New System.Drawing.Point(19, 19)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(332, 86)
+        Me.Panel7.TabIndex = 2
+        '
+        'TextSearch
+        '
+        Me.TextSearch.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextSearch.Location = New System.Drawing.Point(16, 12)
+        Me.TextSearch.Name = "TextSearch"
+        Me.TextSearch.Size = New System.Drawing.Size(316, 61)
+        Me.TextSearch.TabIndex = 1
+        Me.ToolTip1.SetToolTip(Me.TextSearch, "Search")
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.SkyBlue
+        Me.Panel6.Controls.Add(Me.btnExit)
+        Me.Panel6.Controls.Add(Me.btnReset)
+        Me.Panel6.Controls.Add(Me.btnDelete)
+        Me.Panel6.Controls.Add(Me.btnUpdate)
+        Me.Panel6.Controls.Add(Me.btnAdd)
+        Me.Panel6.Location = New System.Drawing.Point(19, 124)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(332, 463)
+        Me.Panel6.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Gray
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(16, 383)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(313, 69)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.ToolTip1.SetToolTip(Me.btnExit, "Exit System")
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.Color.Peru
+        Me.btnReset.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnReset.ForeColor = System.Drawing.Color.White
+        Me.btnReset.Location = New System.Drawing.Point(16, 294)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(313, 76)
+        Me.btnReset.TabIndex = 0
+        Me.btnReset.Text = "Reset"
+        Me.ToolTip1.SetToolTip(Me.btnReset, "Reset Form")
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.DarkRed
+        Me.btnDelete.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDelete.ForeColor = System.Drawing.Color.White
+        Me.btnDelete.Location = New System.Drawing.Point(16, 205)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(313, 74)
+        Me.btnDelete.TabIndex = 0
+        Me.btnDelete.Text = "Delete"
+        Me.ToolTip1.SetToolTip(Me.btnDelete, "Delete Book")
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.Green
+        Me.btnUpdate.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnUpdate.ForeColor = System.Drawing.Color.White
+        Me.btnUpdate.Location = New System.Drawing.Point(19, 116)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(313, 74)
+        Me.btnUpdate.TabIndex = 0
+        Me.btnUpdate.Text = "Update Book"
+        Me.ToolTip1.SetToolTip(Me.btnUpdate, "Update Book")
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.BackColor = System.Drawing.Color.DarkBlue
+        Me.btnAdd.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnAdd.ForeColor = System.Drawing.Color.White
+        Me.btnAdd.Location = New System.Drawing.Point(19, 20)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(316, 82)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "Add Book"
+        Me.ToolTip1.SetToolTip(Me.btnAdd, "Add Book")
+        Me.btnAdd.UseVisualStyleBackColor = False
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Document = Me.PrintDocument1
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1332, 773)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.DataTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel7.ResumeLayout(False)
+        Me.Panel7.PerformLayout()
+        Me.Panel6.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents SystemName As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents DataTable As DataGridView
+    Friend WithEvents TextPrice As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextAuthor As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextTitle As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel7 As Panel
+    Friend WithEvents TextSearch As TextBox
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.Win32
+
+Public Class Form1
+
+    Dim sqlConn As New MySqlConnection
+    Dim sqlCommand As New MySqlCommand
+    Dim sqlRead As MySqlDataReader
+    Dim sqlDataTable As New DataTable
+    Dim data As New MySqlDataAdapter
+
+    Dim sqlQuerry As String
+    Dim server As String = "localhost"
+    Dim username As String = "root"
+    Dim password As String = ""
+    Dim database As String = "visual_basic"
+
+    Dim id As Integer
+
+    Private bitmap As Bitmap
+
+    Private Sub updateTable()
+        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database
+        sqlConn.Open()
+        sqlCommand.Connection = sqlConn
+        sqlCommand.CommandText = "SELECT * FROM visual_basic.books"
+        sqlRead = sqlCommand.ExecuteReader
+        sqlDataTable.Load(sqlRead)
+        sqlRead.Close()
+        sqlConn.Close()
+        DataTable.DataSource = sqlDataTable
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        updateTable()
+    End Sub
+
+    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim bExit As DialogResult
+        bExit = MessageBox.Show("Confirm you want to exit", "Book Management System", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If bExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
+
+
+
+    Private Sub DataTable_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataTable.CellClick
+        Try
+            id = DataTable.SelectedRows(0).Cells(0).Value.ToString
+            TextTitle.Text = DataTable.SelectedRows(0).Cells(1).Value.ToString
+            TextAuthor.Text = DataTable.SelectedRows(0).Cells(2).Value.ToString
+            TextPrice.Text = DataTable.SelectedRows(0).Cells(3).Value.ToString
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database
+
+        Try
+            sqlConn.Open()
+            sqlQuerry = "INSERT INTO visual_basic.books(title, author, price) VALUES ('" & TextTitle.Text & "', '" & TextAuthor.Text & "', '" & TextPrice.Text & "')"
+            sqlCommand = New MySqlCommand(sqlQuerry, sqlConn)
+            sqlRead = sqlCommand.ExecuteReader
+            sqlConn.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Book Management System", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Finally
+            sqlConn.Dispose()
+        End Try
+        updateTable()
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        TextTitle.Clear()
+        TextAuthor.Clear()
+        TextPrice.Clear()
+        TextSearch.Clear()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database
+        sqlConn.Open()
+        sqlCommand.Connection = sqlConn
+        sqlCommand.CommandText = "UPDATE visual_basic.books SET title = '" & TextTitle.Text & "', author = '" & TextAuthor.Text & "', price = '" & TextPrice.Text & "' WHERE id = '" & id & "'"
+        sqlCommand.CommandType = CommandType.Text
+        sqlCommand.ExecuteNonQuery()
+        sqlConn.Close()
+        updateTable()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        sqlConn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" + "password=" + password + ";" + "database=" + database
+        sqlConn.Open()
+        sqlCommand.Connection = sqlConn
+        sqlCommand.CommandText = "DELETE FROM books WHERE id = '" & id & "'"
+        sqlCommand.CommandType = CommandType.Text
+        sqlCommand.ExecuteNonQuery()
+        sqlConn.Close()
+
+        For Each row As DataGridViewRow In DataTable.SelectedRows
+            DataTable.Rows.Remove(row)
+        Next
+        updateTable()
+    End Sub
+
+    Private Sub btnPrint_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TextSearch_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextSearch.KeyPress
+        Try
+            If Asc(e.KeyChar) = 13 Then
+                Dim dv As DataView
+                dv = sqlDataTable.DefaultView
+                dv.RowFilter = String.Format("Title like '%{0}%' OR Author like '%{0}%'", TextSearch.Text)
+                DataTable.DataSource = dv.ToTable()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
